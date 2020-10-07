f36d70130b0f863bce16659a13aa5c008bb7fe3c
sxmeer-ahmed <ahmedkhansameer50@gmail.com>
2020-10-07 19:43:35 +0530

    Task1+Task2+Task3 Completed

--- Flutter_app/lib/main.dart
+++ Flutter_app/lib/main.dart
@@ -1,8 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 int D=0;
-String Sameer = "0";
 void main() => runApp(MaterialApp(
   title: 'Click Counter',
   theme: ThemeData(
@@ -18,19 +19,63 @@ class HomeScreen extends StatefulWidget {
   HomeScreenState createState() => HomeScreenState();
 }
 
-class HomeScreenState extends State<HomeScreen> {
+class HomeScreenState extends State<HomeScreen>  with SingleTickerProviderStateMixin{
 
-  List colors = [Colors.red, Colors.green, Colors.yellow,Colors.blue,Colors.orange,Colors.brown];
-  Random random = new Random();
+  int P;
+  final database = Firestore.instance;
+  
+  AnimationController rotationController;
+  void fetchdata()async{
+    var local = await database.document('score/nx5P7Qc9R9tlRfgrYRNu').get();
+    setState(() {
+      P = local.data["Click"];
+    });
+  }
+  void initState() {
+    rotationController = AnimationController(vsync: this, duration: Duration(seconds: 5), upperBound: 2);
+    fetchdata();
 
-  int index = 0;
 
-  void changeIndex() {
-    setState(() => index = random.nextInt(6));
+    super.initState();
   }
 
+
+
+
+  List colors = [Colors.red, Colors.green, Colors.yellow,Colors.blue,Colors.orange,Colors.brown,Colors.black,Colors.pinkAccent,Colors.purpleAccent,Colors.amberAccent];
+  List coll = ['android/assets/4-2-cartoon-transparent.png',
+                     'android/assets/7-2-cartoon-picture.png',
+                     'android/assets/1 (1).png',
+                     'android/assets/1 (2).png',
+                      'android/assets/1 (3).png',
+                      'android/assets/1 (4).png',
+                       'android/assets/1 (5).png',
+                      'android/assets/1 (6).png',
+                       'android/assets/1 (7).png',
+                       'android/assets/1-2-cartoon-png-image.png'];
+
+  Widget animation_widget()
+  {
+    switch(D%2)
+    {
+      case 1: return  RotationTransition(
+          turns: Tween(begin: 0.0, end: -1.0).animate(rotationController),
+          child: Container(
+              child : Image(image : AssetImage(coll[D%10]))
+          ),
+        );
+      case 0:return RotationTransition(
+        turns: Tween(begin: 0.0, end: 1.0).animate(rotationController),
+        child : Container(
+            child : Image(image : AssetImage(coll[D%10]),)
+        ),
+      );
+    }
+  }
+
+
   @override
-  Widget build(BuildContext context) {
+   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
           title: Center(
@@ -38,54 +83,96 @@ class HomeScreenState extends State<HomeScreen> {
           )),
       body: Center(
         child: Container(
-          padding : EdgeInsets.all(30.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              Flexible(
+                  child : animation_widget(),
+                  ),
                 Flexible(
-                child:Center(
-                       child : Text(Sameer,
+                  child : Container(
+                    padding: EdgeInsets.only(top : 30.0),
+                child:Text(D.toString(),
                     style: TextStyle(
-                      fontSize: 150.0,
+                      fontSize: 50.0,
                       fontWeight: FontWeight.bold,
-                      color: colors[index],
+                      color: colors[D%10],
                     ),
                        ),
+                  ),
                 ),
-
+              Flexible(
+                child : Container(
+                  padding: EdgeInsets.only(top : 20.0,bottom: 30.0),
+                  child:Text("Click Count",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      color: colors[(D+1)%10],
+                    ),
+                  ),
                 ),
-
+              ),
 
               Flexible(
-                child : Center(
+                child : Container(
+                  padding: EdgeInsets.only(bottom: 30.0),
                   child: FlatButton(
                     onPressed: () {
-                      changeIndex();
                       setState(() {
                         ++D;
-                        Sameer = D.toString();
+                        database.document('score/nx5P7Qc9R9tlRfgrYRNu').updateData({"Click": D});
+                        animation_widget();
                       });
                     },
                     child: Text("CLICK"),
-                    color: colors[index],
+                    color: colors[(D+2)%10],
                     textColor: Colors.white,
                     ),
               ),
               ),
 
               Flexible(
+                child: FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      rotationController.forward(from: 0.0);
+                    });
+                  },
+                  child: Text("ROTATE"),
+                  color: colors[(D+3)%10],
+                  textColor: Colors.white,
+                ),
+              ),
+              Flexible(
+                child : Container(
+                  padding: EdgeInsets.only(top: 30.0),
                   child: FlatButton(
                     onPressed: () {
-                      changeIndex();
                       setState(() {
                         D = 0;
-                        Sameer = D.toString();
+                        database.document('score/nx5P7Qc9R9tlRfgrYRNu').updateData({"Click": D});
+                        animation_widget();
                       });
                     },
                     child: Text("RESET"),
-                    color: colors[index],
+                    color: colors[(D+4)%10],
                     textColor: Colors.white,
                   ),
+              ),
+              ),
+
+              Flexible(
+                child : Container(
+                  padding: EdgeInsets.only(top: 50.0 ),
+                  child:Text("Last time you clicked : " + P.toString() +" times" ,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      color: colors[(D+5)%10],
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
@@ -93,6 +180,9 @@ class HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
+
 }
+
+
 
 
